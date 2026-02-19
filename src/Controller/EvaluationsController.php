883a7e50be7458342b02f1095f9c8ca65934a39e
Nicolas vauché <hello@nicolasvauche.net>
2026-02-19 14:23:28 +0100

    Feat(Promotion): Sidebar menus

--- src/Controller/EvaluationsController.php
+++ src/Controller/EvaluationsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\Framework\FrameworkJsonLoader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,8 +11,10 @@ use Symfony\Component\Routing\Attribute\Route;
 final class EvaluationsController extends AbstractController
 {
     #[Route('/evaluations', name: 'app_promotion_evaluations')]
-    public function index(Request $request): Response
-    {
+    public function index(
+        Request $request,
+        FrameworkJsonLoader $loader
+    ): Response {
         $promotion = $request->query->get('promotion');
         $year = $request->query->get('year');
 
@@ -19,9 +22,12 @@ final class EvaluationsController extends AbstractController
             return $this->redirectToRoute('app_home');
         }
 
+        $framework = $loader->load($promotion, $year);
+
         return $this->render('evaluations/index.html.twig', [
             'promotion' => $promotion,
             'year' => $year,
+            'framework' => $framework,
         ]);
     }
 }
--- src/Controller/SkillsController.php
+++ src/Controller/SkillsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\Framework\FrameworkJsonLoader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,8 +11,10 @@ use Symfony\Component\Routing\Attribute\Route;
 final class SkillsController extends AbstractController
 {
     #[Route('/competences', name: 'app_promotion_skills')]
-    public function index(Request $request): Response
-    {
+    public function index(
+        Request $request,
+        FrameworkJsonLoader $loader
+    ): Response {
         $promotion = $request->query->get('promotion');
         $year = $request->query->get('year');
 
@@ -19,9 +22,12 @@ final class SkillsController extends AbstractController
             return $this->redirectToRoute('app_home');
         }
 
+        $framework = $loader->load($promotion, $year);
+
         return $this->render('skills/index.html.twig', [
             'promotion' => $promotion,
             'year' => $year,
+            'framework' => $framework,
         ]);
     }
 }
