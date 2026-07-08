e826e3c050653a273eb0bd879ca63bf6b52d22da
Nicolas Vauché <vauche@3il.fr>
2026-07-08 11:24:53 +0200

    feat: User & Auth

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
     Symfonycasts\SassBundle\SymfonycastsSassBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
 ];
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher,
+    )
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $this->createUser(
+            $manager,
+            'vauche@3il.fr',
+            'test',
+            'Nicolas Vauché',
+            ['ROLE_ADMIN'],
+        );
+
+        $this->createUser(
+            $manager,
+            'nvauche@gmail.com',
+            'test',
+            'Utilisateur test',
+            ['ROLE_USER'],
+        );
+
+        $manager->flush();
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    private function createUser(ObjectManager $manager, string $email, string $password, string $fullName, array $roles): void
+    {
+        $user = $manager->getRepository(User::class)->findOneBy(['email' => strtolower($email)]);
+
+        if (!$user instanceof User) {
+            $user = new User();
+            $manager->persist($user);
+        }
+
+        $user
+            ->setEmail($email)
+            ->setFullName($fullName)
+            ->setRoles($roles);
+        $user->setPassword($this->passwordHasher->hashPassword($user, $password));
+    }
+}
